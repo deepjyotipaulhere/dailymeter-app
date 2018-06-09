b67dfddfbf9e739a32ce05aa85a91f4bba714bce
Deepjyoti Paul <deepjyotipaul69@gmail.com>
2018-06-09 18:43:42 +0530

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <Page style="background-color:red">
+
+    <StackLayout>
+      <TextField :text="textFieldValue" hint="Username" style="color:white;padding:10px" />
+      <Button class="btn btn-primary" @tap="$router.push('/hello')">Hello World</Button>
+    </StackLayout>
+
+  </Page>
+</template>
